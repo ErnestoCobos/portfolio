3547338aff6b17ce154aa0f0e2cc8ae6758d241d
Ernesto Cobos <ernesto@cobos.io>
2024-09-02 11:15:30 -0600

    Update project dependencies and Sentry configuration
    Changed `org` and `project` fields in `next.config.mjs` for better alignment. Added `@next/third-parties` dependency and integrated Google Analytics in `app/layout.tsx`. Updated Sentry DSN for enhanced error tracking.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -31,6 +32,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <SpeedInsights />
       </body>
       <GoogleAnalytics gaId="G-FG8NBYLPH8" />
     </html>
