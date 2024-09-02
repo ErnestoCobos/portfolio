a2aea2e43e7edf8486de795244e1259836ee5451
Ernesto Cobos <ernesto@cobos.io>
2024-09-02 11:14:25 -0600

    Update project dependencies and Sentry configuration
    Changed `org` and `project` fields in `next.config.mjs` for better alignment. Added `@next/third-parties` dependency and integrated Google Analytics in `app/layout.tsx`. Updated Sentry DSN for enhanced error tracking.

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://1049854104ae9be4b8a6c9efe65f38ab@o4506980443619328.ingest.us.sentry.io/4507884263309312",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
