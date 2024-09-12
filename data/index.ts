aed37da3849aafaa52e3a37af2f1eb1602bc3628
Ernesto Cobos <ernesto@cobos.io>
2024-09-12 09:09:46 -0600

    Add link to Blog in navigation menu
    Included a new "Blog" item in the navigation menu linking to "https://blog.cobos.io". This enhances site navigation by providing direct access to the blog.

--- data/index.ts
+++ data/index.ts
@@ -3,6 +3,7 @@ export const navItems = [
   { name: "Projects", link: "#projects" },
   // { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
+  { name: "Blog", link: "https://blog.cobos.io" },
 ];
 
 export const gridItems = [
